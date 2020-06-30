0b9259fd5cc9ece10a7d40bd7a401dfcac6213c6
savalone47 <savalonescott47@gmail.com>
2020-06-30 20:18:02 +0200

    add dynamic GET route

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 const mockUserData = [
-	{ name: 'Mark'},
-	{ name: 'Jil'}
+	{ name: 'mark'},
+	{ name: 'jil'}
 ]
-
+// Get Home page
+app.get('/', (req, res) => {
+	res.json({
+		message: 'successfully'
+	});
+});
+// Get User message
 app.get('/users', (req, res)=>{
 	res.json({
 		success: true,
@@ -15,5 +21,15 @@ app.get('/users', (req, res)=>{
 		users: mockUserData
 	});
 });
+// Get User id section
+app.get('/users/:id', (req, res)=>{
+	console.log(req.params.id);
+	res.json({
+		success: true,
+		message: 'got one user',
+		users: req.params.id
+	});
+});
 
+// Running the server
 app.listen(PORT ,() => console.log(`server is running on ${PORT}`));
