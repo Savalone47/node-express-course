813415002f1600fe88dc00a4537562569c2165e3
savalone47 <savalonescott47@gmail.com>
2020-06-30 20:37:08 +0200

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+
 
 const PORT = process.env.PORT || 8000;
 
@@ -7,6 +10,11 @@ const mockUserData = [
 	{ name: 'mark'},
 	{ name: 'jil'}
 ]
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Get Home page
 app.get('/', (req, res) => {
 	res.json({
@@ -29,6 +37,29 @@ app.get('/users/:id', (req, res)=>{
 		message: 'got one user',
 		users: req.params.id
 	});
+});
+
+
+app.post('/login', (req, res)=>{
+
+	const username = req.body.username;
+	const password = req.body.password;
+
+	const mockUsername = "savalone47";
+	const mockPassword = "secret";
+
+	if(username===mockUsername && password===mockPassword){
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		});
+	}else{
+		res.json({
+			success: true,
+			message: 'password and password do not match!'
+		});
+	}
 });
 
 // Running the server
